3aac3ab3e1758f32b1eb329b12b8dcab9ff2501a
Alex <103459735+torn80beta@users.noreply.github.com>
2023-06-15 10:57:14 +0300

    final

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const { contactsPath } = require("./contacts");
+const contacts = require('./contacts');
+const { Command } = require('commander');
+const program = new Command();
 
-console.log(contactsPath);
-// console.log("Hello world!");
-// console.log("I'm your personal demon :) ");
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+    case 'get':
+      const contact = await contacts.getContactById(id);
+      return console.table(contact);
+    case 'remove':
+      const removedContact = await contacts.removeContact(id);
+      return console.table(removedContact);
+    case 'add':
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.table(newContact);
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+      break;
+  }
+};
+
+invokeAction(argv);
